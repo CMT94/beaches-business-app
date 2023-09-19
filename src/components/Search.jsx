f1f531c7d36f031fb006911a5c302763871a8f4d
Clément M <cmt94.dev@gmail.com>
2023-09-19 18:04:55 +0200

    init search section

---
+++ src/components/Search.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { RiCustomerService2Fill } from "react-icons/ri";
+import { MdOutlineTravelExplore } from "react-icons/md";
+
+const Search = () => {
+  return (
+    <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3 gap-4 px-4 py-16">
+      Search
+    </div>
+  );
+};
+
+export default Search;
