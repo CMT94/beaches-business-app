d48768d046305a7f60efc7a5e6c803bc4dd8a45b
Clément M <cmt94.dev@gmail.com>
2023-09-19 08:27:41 +0200

    store pics and hero video in local dir assets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
 };
 
 export default App;
